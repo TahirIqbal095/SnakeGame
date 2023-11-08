65e647e907d3889c34abb57539b3bd2efc301bce
Tahir iqbal <shahtahir786@gmail.com>
2023-11-08 15:14:39 +0530

    snake game not done yet

---
+++ README.md
@@ -0,0 +1,2 @@
+# Snake Game
+- Build in java
---
+++ src/Game/SnakeGame.java
@@ -0,0 +1,81 @@
+package Game;
+
+import java.awt.*;
+import java.awt.event.*;
+import java.util.ArrayList;
+import java.util.Random;
+import javax.swing.*;
+
+public class SnakeGame extends JPanel implements ActionListener{
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        repaint();
+    }
+
+    private class Tile {
+        int x;
+        int y;
+
+        public Tile(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+    int boardWidth;
+    int boardHeight;
+    int tileSize = 25;
+
+    // snake
+    Tile snakeHead;
+
+    // food
+    Tile food;
+    Random random;
+
+    // game logic
+    Timer gameLoop;
+
+    public SnakeGame(int boardWidth, int boardHeight) {
+        this.boardHeight = boardHeight;
+        this.boardWidth = boardWidth;
+
+        setPreferredSize(new Dimension(this.boardWidth, this.boardHeight));
+        setBackground(Color.black);
+
+        snakeHead = new Tile(5, 5);
+
+        food = new Tile(10, 10);
+        random = new Random();
+        placeFood();
+
+        gameLoop = new Timer(100, this);
+        gameLoop.start();
+    }
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        draw(g);
+    }
+
+    public void draw(Graphics g) {
+        for(int i = 0; i < boardWidth / tileSize; i++) {
+            g.drawLine(i * tileSize, 0, i * tileSize, boardHeight);
+            g.drawLine(0, i * tileSize, boardWidth, i * tileSize);
+
+        }
+
+        //food
+        g.setColor(Color.red);
+        g.fillRect(food.x * tileSize, food.y * tileSize, tileSize, tileSize);
+
+        //snake
+        g.setColor(Color.green);
+        g.fillRect(snakeHead.x * tileSize, snakeHead.y * tileSize, tileSize, tileSize);
+    }
+
+    public void placeFood() {
+        food.x = random.nextInt(boardWidth / tileSize); // 600 / 25 = 24
+        food.y = random.nextInt(boardHeight / tileSize);
+    }
+}
